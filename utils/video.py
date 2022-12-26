0d07be8b91dbb95d7c48f5af9620fe50dd67931b
JJLEE-20194099 <long.lt194099@gmail.com>
2022-12-26 16:02:37 +0700

    add play video function

--- utils/video.py
+++ utils/video.py
@@ -8,3 +8,17 @@ def get_video_frames(video_path):
     for frame in video_read:
         video_list.append(frame)
     return video_list
+
+def play_video(video_path, fps=30):
+    cap = cv2.VideoCapture(video_path)
+
+    while(cap.isOpened()):
+        _, frame = cap.read()
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cv2.imshow('frame', gray_frame)
+
+        if (cv2.waitKey(1000/fps) & 0xFF == ord('q')):
+            break
+    
+    cap.release()
+    cv2.destroyAllWindows()
