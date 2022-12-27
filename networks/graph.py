f4e577885da730f47b571d7965827cba7fd3312f
JJLEE-20194099 <long.lt194099@gmail.com>
2022-12-27 15:53:14 +0700

    build graph - base constructure for network

---
+++ networks/graph.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+class Graph():
+    def __init__(
+        self,
+        max_hop = 1,
+        dilation = 1
+    ):
+        self.max_hop = max_hop
+        self.dialation = dilation
+    
+    
