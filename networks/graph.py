3334a3bcff5c1a0cc61d94c9b32815b2d8efcaed
JJLEE-20194099 <long.lt194099@gmail.com>
2022-12-27 16:05:37 +0700

    save edge by openpose-18keypoints

--- networks/graph.py
+++ networks/graph.py
@@ -9,4 +9,16 @@ class Graph():
         self.max_hop = max_hop
         self.dialation = dilation
     
-    
+    def get_edge(self):
+        self.num_node = 18
+        self.link = [(i, i) for i in range(self.num_node)]
+
+        neighbor_link = [(4, 3), (3, 2), (7, 6), (6, 5), (13, 12), (12,
+                                                                        11),
+                             (10, 9), (9, 8), (11, 5), (8, 2), (5, 1), (2, 1),
+                             (0, 1), (15, 0), (14, 0), (17, 15), (16, 14)]
+        
+        self.edge = self.link + neighbor_link
+
+        self.center = 1
+        
